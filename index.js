d7fde582388990a822a5505304662dd07531d95b
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-05 03:03:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
